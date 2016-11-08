f9e5a8f997d4798ed54fe23a78acc9c612e58372
JoshNorthrup <JoshNorthrup85@gmail.com>
2016-11-07 21:28:13 -0400

    clockin query

---
+++ src/main/java/ClockDbHandler.java
@@ -0,0 +1,38 @@
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.sql.TIMESTAMP;
+
+import java.sql.*;
+
+/**
+ * Created by Josh on 2016-11-06.
+ */
+public class ClockDbHandler {
+    Connection con;
+    public ClockDbHandler(){
+        con = null;
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            con = DriverManager.getConnection("jdbc:oracle:thin:@"+DBVar.DEV_URL+":"+DBVar.DEV_PORT+":"+DBVar.DEV_SID,DBVar.DEV_USERNAME,DBVar.DEV_PASSWORD);
+
+        }catch(Exception e){        }
+    }
+
+    public boolean signIn(int employee_id, int shift_id, int location_id){
+        try {
+            OraclePreparedStatement stmt = (OraclePreparedStatement) con.prepareStatement(
+                    "INSERT INTO worked_shifts(start_time,scheduled_shift_ID,employee_ID, location_ID) VALUES (?,?,?,?)");
+            stmt.setTIMESTAMP(1, new TIMESTAMP(new Date(System.currentTimeMillis())));
+            stmt.setInt(2, shift_id);
+            stmt.setInt(3, employee_id);
+            stmt.setInt(4, location_id);
+            int i = stmt.executeUpdate();
+            if (i > 0)
+                return true;
+
+        }catch(Exception e){
+            return false;
+        }
+        return false;
+    }
+}
